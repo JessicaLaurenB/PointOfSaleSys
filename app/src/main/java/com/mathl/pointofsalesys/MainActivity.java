21b6e3a7c4e062883132e2d8d728d80d4bf00f5a
Neal <nealnguyen@knights.ucf.edu>
2016-08-14 12:49:33 -0400

    make main activity full screen

--- app/src/main/java/com/mathl/pointofsalesys/MainActivity.java
+++ app/src/main/java/com/mathl/pointofsalesys/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -35,6 +37,7 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         final ArrayList<String> buttons = new ArrayList<String>();
         final GridView employeeGrid = (GridView) findViewById(R.id.gridView);
